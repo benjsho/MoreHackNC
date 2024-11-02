de425f6bdfb18139edca2ab56296e6ef33ecceec
S33Bb <sebastiancolemalhotra@gmail.com>
2024-11-02 17:47:50 -0400

    screwy push

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,11 +5,14 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 var resources = 0;
+var pickaxeLevel = 1;
 @onready var resourceCounter = $UI/ResourceLabel
 
 var canMine: bool = false
+var canTalk: bool = false
 
 @onready var pivot: Node3D = $Pivot
+@onready var animPlayer: AnimationPlayer = $AnimationPlayer
 
 @export_category("Sensitivity")
 @export var xSens: float = 0.5
@@ -58,22 +61,34 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _on_rock_area_entered(area:Area3D) -> void:
-	canMine = true;
-	print_debug("Can mine")
+	if (area.is_in_group("player")):
+		canMine = true;
+		print_debug("Can mine")
 
 
 func _on_rock_area_exited(area:Area3D) -> void:
-	canMine = false;
-	print_debug("Can't mine")
+	if (area.is_in_group("player")):
+		canMine = false;
+		print_debug("Can't mine")
 		
 func updateResources() -> void:
 	resourceCounter.text = "Resources: " + str(resources)
-	
+
+func mine() -> void:
+	match pickaxeLevel:
+		1:
+			resources += 0.01
+		2:
+			resources += 10
+		3:
+			resources += 500
 
 
 func _on_npc_area_entered(area: Area3D) -> void:
-	pass # Replace with function body.
+	if (area.is_in_group("player")):
+		canTalk = true;
 
 
 func _on_npc_area_exited(area: Area3D) -> void:
-	pass # Replace with function body.
+	if (area.is_in_group("player")):
+		canTalk = false;

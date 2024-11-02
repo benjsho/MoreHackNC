2df095b29d15c67e0059aa737b3bb4808d365f81
S33Bb <sebastiancolemalhotra@gmail.com>
2024-11-02 14:17:12 -0400

    THE MENU IS WORKING!!! NOT CLICKBAIT (GONE SNOEYINK)

--- scenes/menu_nc.gd
+++ scenes/menu_nc.gd
@@ -7,8 +7,9 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_anything_pressed():
-		get_tree().change_scene_to_file("res://world.tscn")
+		get_tree().change_scene_to_file("res://scenes/spaceworld.tscn")
 	pass

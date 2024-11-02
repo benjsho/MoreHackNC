f4f9b657e089b23102e9a47fe9445dfdb9de5ddf
S33Bb <sebastiancolemalhotra@gmail.com>
2024-11-02 13:55:20 -0400

    Added pickaxe, reoriented stuff

--- scripts/player.gd
+++ scripts/player.gd
@@ -16,21 +16,28 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(clamp(-event.relative.x * xSens, -30, 90)))
+		rotate_y(deg_to_rad(-event.relative.x * xSens))
 		pivot.rotate_x(clamp((deg_to_rad(-event.relative.y * ySens)), -30, 90))
+		pivot.rotation.x = clamp(pivot.rotation.x, -90, 90)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	if Input.is_action_just_pressed("Escape"):
+		if Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		elif Input.MOUSE_MODE_VISIBLE:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED

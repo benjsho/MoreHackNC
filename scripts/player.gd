4123376763116c4815dd46ce1cb348689cddb93f
Ben Shoenbill <benjo@ad.unc.edu>
2024-11-02 22:06:35 -0400

    dialogue saved

--- scripts/player.gd
+++ scripts/player.gd
@@ -50,8 +50,8 @@ func _physics_process(delta: float) -> void:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 			
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
 
 	if Input.is_action_just_pressed("Mine") and canMine and pickaxeEnabled:
 		animPlayer.play("pickaxeSwing")
@@ -62,25 +62,30 @@ func _physics_process(delta: float) -> void:
 		print_debug("pickWhiff")
 	
 
-	if Input.is_action_just_pressed("Interact") and canTalk:
+	if Input.is_action_just_pressed("ui_accept") and canTalk:
 		match npcTalk:
 			1:
 				if (resources >= 100):
 					jerryLevel += 1;
 				match jerryLevel:
 					0:
-						Dialogic.start("introWithJerry")
-						jerryLevel += 1;
+						if Dialogic.current_timeline == null:
+							Dialogic.start("introWithJerry")
+							jerryLevel += 1;
+							dealLevel = 1; 
 					1:
-						Dialogic.start("jerryDismissal")
+						if Dialogic.current_timeline == null:
+							Dialogic.start("jerryDismissal")
 					_:
 						print_debug("Error" + str(jerryLevel))
 
 			2:
 				match dealLevel:
-					0:
-						print_debug("Talking with the guy")
-					1: 
+					1:
+						if Dialogic.current_timeline == null:
+							print_debug("Talking with the guy")
+							Dialogic.start("mysterydialogue")
+					5: 
 						pass
 					_:
 						print_debug("Error" + str(dealLevel))
@@ -153,4 +158,3 @@ func _on_dialogic_signal(argument:String):
 		badPickaxe.visible = true;
 	else:
 		print_debug("error")
-
